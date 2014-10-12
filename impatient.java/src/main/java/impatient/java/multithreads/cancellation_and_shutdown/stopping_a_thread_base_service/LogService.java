2fc7d1d579e31585cb801a2d80f563f1d17ebfc2
EHNGXNG <xianguang.huang@ericsson.com>
2014-10-13 07:08:03 +0800

    update

---
+++ impatient.java/src/main/java/impatient/java/multithreads/cancellation_and_shutdown/stopping_a_thread_base_service/LogService.java
@@ -0,0 +1,85 @@
+/*------------------------------------------------------------------------------
+ * COPYRIGHT Ericsson 2014
+ *
+ * The copyright to the computer program(s) herein is the property of
+ * Ericsson Inc. The programs may be used and/or copied only with written
+ * permission from Ericsson Inc. or in accordance with the terms and
+ * conditions stipulated in the agreement/contract under which the
+ * program(s) have been supplied.
+ *----------------------------------------------------------------------------*/
+package impatient.java.multithreads.cancellation_and_shutdown.stopping_a_thread_base_service;
+
+import java.io.PrintWriter;
+import java.util.concurrent.BlockingQueue;
+
+import net.jcip.annotations.GuardedBy;
+
+/*
+ * 
+ * 使用同步保证log的cancel
+ * */
+public class LogService {
+
+    private final BlockingQueue<String> queue;
+    private final LoggerThread loggerThread;
+    private final PrintWriter writer;
+
+    @GuardedBy("this")
+    private boolean isShutdown;
+
+    @GuardedBy("this")
+    private int reservations;
+
+    public LogService(BlockingQueue<String> queue, LoggerThread loggerThread, PrintWriter writer) {
+        this.queue = queue;
+        this.loggerThread = loggerThread;
+        this.writer = writer;
+    }
+
+    public void start() {
+        loggerThread.start();
+    }
+
+    public void stop() {
+        synchronized (this) {
+            isShutdown = true;
+        }
+        loggerThread.interrupt();
+    }
+
+    public void log(String msg) throws InterruptedException {
+        synchronized (this) {
+            if (isShutdown) {
+                throw new IllegalStateException();
+            }
+            ++reservations;
+        }
+        queue.put(msg);
+    }
+
+    private class LoggerThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    synchronized (LogService.this) {
+                        if (isShutdown && (reservations == 0)) {
+                            break;
+                        }
+                    }
+                    String msg = queue.take();
+                    synchronized (LogService.this) {
+                        --reservations;
+                    }
+                    writer.println(msg);
+                }
+            } catch (InterruptedException e) {
+                /* retry */
+            } finally {
+                writer.close();
+
+            }
+        }
+    }
+
+}
---
+++ impatient.java/src/main/java/impatient/java/multithreads/cancellation_and_shutdown/stopping_a_thread_base_service/LogServiceExecutorService.java
@@ -0,0 +1,79 @@
+/*------------------------------------------------------------------------------
+ * COPYRIGHT Ericsson 2014
+ *
+ * The copyright to the computer program(s) herein is the property of
+ * Ericsson Inc. The programs may be used and/or copied only with written
+ * permission from Ericsson Inc. or in accordance with the terms and
+ * conditions stipulated in the agreement/contract under which the
+ * program(s) have been supplied.
+ *----------------------------------------------------------------------------*/
+package impatient.java.multithreads.cancellation_and_shutdown.stopping_a_thread_base_service;
+
+import java.io.PrintWriter;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import net.jcip.annotations.GuardedBy;
+
+/*
+ * 
+ * 使用同步保证log的cancel
+ * */
+public class LogServiceExecutorService {
+
+    private static final long TIMEOUT = 0;
+    private static final TimeUnit UNIT = TimeUnit.SECONDS;
+    private final ExecutorService exec = Executors.newSingleThreadExecutor();
+    private final PrintWriter writer;
+
+    @GuardedBy("this")
+    private boolean isShutdown;
+
+    @GuardedBy("this")
+    private int reservations;
+
+    public LogServiceExecutorService(PrintWriter writer) {
+        this.writer = writer;
+    }
+
+    public void start() {
+
+    }
+
+    /*
+     * 使用Thread Pool executor 来关闭线程池这照哦给你方式更加优雅
+     */
+    public void stop() throws InterruptedException {
+        try {
+            exec.shutdown();
+            exec.awaitTermination(TIMEOUT, UNIT);
+        } finally {
+            writer.close();
+        }
+    }
+
+    public void log(String msg) throws InterruptedException {
+        exec.execute(new WriterTask(msg));
+    }
+
+    private class WriterTask implements Runnable {
+
+        private final String message;
+
+        WriterTask(String message) {
+            this.message = message;
+        }
+
+        @Override
+        public void run() {
+            try {
+                writer.println(message);
+            } finally {
+                writer.close();
+
+            }
+        }
+    }
+
+}
