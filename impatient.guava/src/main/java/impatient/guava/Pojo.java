8cef9def8cbe4c953cf25b70fe3885a6b6a03af4
XianGuang Huang <87225705@qq.com>
2015-03-15 22:30:24 +0800

    add guava example

---
+++ impatient.guava/src/main/java/impatient/guava/Pojo.java
@@ -0,0 +1,27 @@
+package impatient.guava;
+
+/**
+ * Created by xianguanghuang on 15-3-15.
+ */
+public class Pojo {
+
+    private int number;
+    private String description;
+
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
