c3d8749c6dc830ca2e28e962384084d207fe36c0
xianguanghuang <huangxianguang@yy.com>
2016-01-05 18:50:49 +0800

    add configuration file

---
+++ impatient.springboot/src/main/java/impatient/springboot/configuration/ApplyPropertiesConfiguration.java
@@ -0,0 +1,34 @@
+package impatient.springboot.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+/**
+ * Created by Administrator on 2016/1/5.
+ */
+//// TODO: 2016/1/5 need add example to verify
+@Configuration
+@PropertySource("application.properties")
+public class ApplyPropertiesConfiguration {
+
+    @Value("${redis.hostname}")
+    private String redisHostName;
+
+    @Value("${redis.port}")
+    private int redisPort;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public  String useHostName(){
+        return redisHostName;
+    }
+
+
+}
