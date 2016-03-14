b269ecf253ab5995e8da2687253af098862444dc
xianguanghuang <xianguanghuang@outlook.com>
2016-03-14 23:35:07 +0800

    增加Spring stand alone application

---
+++ impatient.springboot.standalone/src/main/java/org/xcraftman/RedisThread.java
@@ -0,0 +1,29 @@
+package org.xcraftman;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * Created by xianguanghuang on 2016/3/14. For Note and Demo purpose
+ */
+public class RedisThread implements Runnable{
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RedisThread(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+
+    @Override
+    public void run() {
+        for (int i = 0 ; i < 10000; i ++){
+            stringRedisTemplate.opsForValue().append("b","bb");
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("thread run done");
+    }
+}
