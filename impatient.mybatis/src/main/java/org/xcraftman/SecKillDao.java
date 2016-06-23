c41e521cf375c66c70aadf3eb3861df43dba0084
xianguanghuang <xianguanghuang@outlook.com>
2016-06-23 23:04:48 +0800

    添加mybatis 模块

---
+++ impatient.mybatis/src/main/java/org/xcraftman/SecKillDao.java
@@ -0,0 +1,11 @@
+package org.xcraftman;
+
+import java.util.Map;
+
+/**
+ * Created by xianguanghuang on 2016/6/23. For Note and Demo purpose
+ */
+public interface SecKillDao {
+
+    void killByProcedure(Map<String, Object> paramMap);
+}
