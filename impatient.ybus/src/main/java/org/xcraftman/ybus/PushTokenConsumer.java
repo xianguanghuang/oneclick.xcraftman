9e111ad03c024b2bca74cb860aa7ea6623c5b06a
xianguanghuang <huangxianguang@yy.com>
2018-03-19 21:43:47 +0800

    增加ybus 例子

---
+++ impatient.ybus/src/main/java/org/xcraftman/ybus/PushTokenConsumer.java
@@ -0,0 +1,31 @@
+package org.xcraftman.ybus;
+
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class PushTokenConsumer {
+
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "dmq-test.yy.com:9099");
+        props.put("group.id", "pushtoken.group");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("sub_svPushTokenTopic"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+        }
+    }
+
+}
